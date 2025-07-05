bb1853b1b52381b61de6dfe868e8ed3a8b88fdd0
Hryniuk_Taras <gritaras484@gmail.com>
2025-07-05 09:38:22 +0300

    task

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,62 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  let condition;
+  let first;
+  let second;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    condition = jest.fn();
+    first = jest.fn();
+    second = jest.fn();
   });
 
-  // write tests here
+  const { ifElse } = require('./ifElse');
+
+  it('should be a function', () => {
+    expect(typeof ifElse).toBe('function');
+  });
+
+  it('should return undefined', () => {
+    expect(ifElse(condition, first, second)).toBeUndefined();
+  });
+
+  it('should receive 3 callbacks', () => {
+    expect(typeof condition).toBe('function');
+    expect(typeof first).toBe('function');
+    expect(typeof second).toBe('function');
+    expect(ifElse).toHaveLength(3);
+  });
+
+  it('calls "first" cb with no arguments when condition is true', () => {
+    condition.mockReturnValue(true);
+
+    ifElse(condition, first, second);
+
+    // Condition should be called once
+    expect(condition).toHaveBeenCalledTimes(1);
+    // Condition should be called with no arguments
+    expect(condition).toHaveBeenCalledWith();
+
+    // First should be called once
+    expect(first).toHaveBeenCalledTimes(1);
+    // First should not be called with arguments
+    expect(first).toHaveBeenCalledWith();
+  });
+
+  it('calls "second" cb with no arguments when condition is false', () => {
+    condition.mockReturnValue(false);
+
+    ifElse(condition, first, second);
+
+    // Condition should be called once
+    expect(condition).toHaveBeenCalledTimes(1);
+    // Condition should be called with no arguments
+    expect(condition).toHaveBeenCalledWith();
+
+    // Second should be called once
+    expect(second).toHaveBeenCalledTimes(1);
+    // Second should not be called with arguments
+    expect(second).toHaveBeenCalledWith();
+  });
 });
